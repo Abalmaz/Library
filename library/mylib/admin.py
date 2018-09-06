19c2a90cb1e347e191181b3e383d21889d5f6895
Abalmazik <yana.abalmaz@gmail.com>
2018-09-06 10:57:16 +0300

    Add base view and templates for books

--- library/mylib/admin.py
+++ library/mylib/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models  import Book, Author, PublishingHouse, Genre, Country
 
 # Register your models here.
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(PublishingHouse)
+admin.site.register(Genre)
+admin.site.register(Country)
