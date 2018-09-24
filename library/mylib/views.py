83667fb603533e380bd249f96cd14781043a2897
Abalmazik <yana.abalmaz@gmail.com>
2018-09-24 14:52:28 +0300

    Add add and delete book for publisher with permission mixins

--- library/mylib/views.py
+++ library/mylib/views.py
@@ -1,13 +1,16 @@
 from django.contrib.auth.forms import SetPasswordForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+
 from django.urls import reverse_lazy
 from django.shortcuts import redirect, get_object_or_404, render
 from django.contrib.auth import login
+
+from .mixins import PublisherRequiredMixin, OwnerRequiredMixin
 from .filters import BookFilter
 from .forms import SignUpForm
 from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
 
-from .models import Book, Author, User, Invitation
+from .models import Book, Author, User, Invitation, PublishingHouse
 
 
 class AuthorDetailView(DetailView):
@@ -36,13 +39,22 @@ class BookListView(ListView):
         return BookFilter(self.request.GET, queryset=Book.objects.all()).qs
 
 
-class BookCreateView(CreateView):
+class BookCreateView(PublisherRequiredMixin, CreateView):
+    login_url = 'login'
     model = Book
     template_name = 'mylib/add_book.html'
     fields = ('title', 'authors', 'genre', 'year', 'number_page', 'description', 'cover')
+    success_url = reverse_lazy('profile')
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.publishing = PublishingHouse.objects.get(pk=self.request.user.publisher_profile.pk)
+        obj.save()
+        return super().form_valid(form)
 
 
-class BookDeleteView(DeleteView):
+class BookDeleteView(PublisherRequiredMixin, OwnerRequiredMixin, DeleteView):
+    login_url = 'login'
     model = Book
     success_url = reverse_lazy('profile')
 
