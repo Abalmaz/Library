ba6aa8258c9b3581a35f61706ab93e8fbfec6b76
Abalmazik <yana.abalmaz@gmail.com>
2018-10-05 12:29:41 +0300

    Added files and changes required for deployment to heroku5

---
+++ library/staticfiles/mylib/js/script.80c8a749d83b.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#comments").on("click", ".reply", function(event){
+        event.preventDefault();
+        var form = $("#postcomment").clone(true);
+        form.find('.parent').val($(this).parent().parent().attr('id'));
+        $(this).parent().append(form);
+    });
+});
