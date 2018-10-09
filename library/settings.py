e26798236eab50e0b5a63fb4e360429b102ff95a
Abalmazik <yana.abalmaz@gmail.com>
2018-10-09 09:08:35 +0300

    Change settings to send email

--- library/settings.py
+++ library/settings.py
@@ -148,10 +148,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'mylib/static'),
-#     os.path.join(BASE_DIR, 'static'),
-# )
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mylib/media/')
@@ -160,9 +156,6 @@ FIXTURE_DIRS = [
     os.path.join(BASE_DIR, '/mylib/fixtures/'),
 ]
 
-# TEMPLATE_DIRS = (
-#     os.path.join(BASE_DIR, 'mylib/templates/'),
-# )
 
 AUTH_USER_MODEL = 'mylib.User'
 
@@ -181,11 +174,9 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_SSL = False
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_EMAIL_HOST_USER = 'poletehnika.planshet@gmail.com'
-EMAIL_HOST_PASSWORD = 'pass'
-# EMAIL_HOST = 'localhost'
-# EMAIL_PORT = '1025'
-# EMAIL_FROM = 'library.admin@localhost'
+EMAIL_HOST_USER = 'poletehnika.planshet@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ['SECRET']
+
 INVITATIONS_LIFETIME = 1
 
 
