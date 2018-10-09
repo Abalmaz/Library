efeeea56b102836ba3ff1081963832706447d0ab
Abalmazik <yana.abalmaz@gmail.com>
2018-10-09 16:23:02 +0300

    Change celery brocker to redis, add dockerfile

--- library/settings.py
+++ library/settings.py
@@ -217,8 +217,12 @@ SOCIALACCOUNT_PROVIDERS = {
 }
 
 # Celery settings
-
-CELERY_BROKER_URL = 'amqp://localhost'
+# CELERY STUFF
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
 
 CELERY_BEAT_SCHEDULE = {
     # Executes every Friday morning at 10:30 a.m.
