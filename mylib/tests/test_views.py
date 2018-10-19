71224eecb66df954dd03c119b0131e0da69cddb9
Abalmazik <yana.abalmaz@gmail.com>
2018-10-19 15:14:56 +0300

    Add author detail tests

--- mylib/tests/test_views.py
+++ mylib/tests/test_views.py
@@ -2,8 +2,8 @@ from django.core.management import call_command
 from django.test import TestCase
 from django.urls import reverse, resolve
 
-from mylib.models import Book, User, Author, Genre
-from mylib.views import BookListView, BookDetailView
+from mylib.models import Book, Author
+from mylib.views import BookListView, BookDetailView, AuthorDetailView
 
 
 class BookListTest(TestCase):
@@ -51,13 +51,33 @@ class BookDetailTest(TestCase):
         self.assertEquals(response.status_code, 404)
 
 
-
-
-
-
-
 class AuthorDetailTest(TestCase):
-    pass
+    def setUp(self):
+        call_command('loaddata',
+                     'country.json',
+                     'genre.json',
+                     'PublishingHouse.json',
+                     'author.json',
+                     'book.json',
+                     'm2m.json')
+        self.author = Author.objects.get(id=1)
+        url = reverse('author_info', kwargs={'pk': 1})
+        self.response = self.client.get(url)
+
+    def test_author_detail_view_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_author_detail_view_function(self):
+        view = resolve('/authors/1/')
+        self.assertEquals(view.func.view_class, AuthorDetailView)
+
+    def test_author_detail_view_uses_correct_template(self):
+        self.assertTemplateUsed(self.response, 'mylib/author_detail.html')
+
+    def test_author_detail_view_not_found_status_code(self):
+        url = reverse('author_info', kwargs={'pk': 99999})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 404)
 
 
 class InvitationTest(TestCase):
