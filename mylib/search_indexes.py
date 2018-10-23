50d1c84034d1ea45b15cb8d98365b35fc389b69d
Abalmazik <yana.abalmaz@gmail.com>
2018-10-23 17:18:30 +0300

    Add search by genre

--- mylib/search_indexes.py
+++ mylib/search_indexes.py
@@ -1,11 +1,17 @@
 from haystack import indexes
+from haystack.fields import MultiValueField
+
 from mylib.models import Author, Book
 
 
 class BookIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True,
-                             template_name="search/book_text.txt"
-                             )
+                             template_name="search/book_text.txt")
+
+    genre = MultiValueField()
+
+    def prepare_genre(self, obj):
+        return [genre.pk for genre in obj.genre.all()]
 
     def get_model(self):
         return Book
@@ -14,6 +20,10 @@ class BookIndex(indexes.SearchIndex, indexes.Indexable):
 class AuthorIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True,
                              template_name="search/author_text.txt")
+    genre = MultiValueField()
+
+    def prepare_genre(self, obj):
+        return [genre.pk for genre in obj.genre.all()]
 
     def get_model(self):
         return Author
