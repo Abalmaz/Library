9b17ac2979034132b25e7e9c7e96efe970c16c88
Yana Abalmaz <yana.abalmaz@gmail.com>
2022-08-01 19:40:41 +0300

    Added clear images functionality

--- script.groovy
+++ script.groovy
@@ -1,11 +1,26 @@
-def buildImage(){
-    echo "building..."
+def clearImage(){
+    sh "docker stop $(docker ps -q) || docker rm $(docker ps -a -q) || docker rmi $(docker images -q | tail -n +2)"
+}
+
+def dockerLogin(){
     withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')])
     {
-        echo "Build the Docker image..."
-        sh "docker build -t $IMAGE_NAME ."
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push $IMAGE_NAME"
+            sh "echo $PASS | docker login -u $USER --password-stdin"
     }
+}
+
+def dockerBuild(String imageName){
+    script.sh "docker build -t $imageName ."
+}
+
+def dockerPush(String imageName){
+    script.sh "docker push $imageName"
+}
+
+def buildImage(){
+    clearImage()
+    dockerLogin()
+    dockerBuild(IMAGE_NAME)
+    dockerPush(IMAGE_NAME)
 }
 return this
--- terraform/modules/webserver/main.tf
+++ terraform/modules/webserver/main.tf
@@ -10,9 +10,9 @@ resource "aws_security_group" "myapp-sg" {
   }
 
   ingress {
-    from_port = 8080
+    from_port = 80
     protocol  = "tcp"
-    to_port   = 8080
+    to_port   = 80
     cidr_blocks = ["0.0.0.0/0"]
   }
 
