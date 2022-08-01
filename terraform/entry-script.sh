a4c7c01ea80d8d0fcb6a652ce12a50636ab21c19
Yana Abalmaz <yana.abalmaz@gmail.com>
2022-08-01 16:00:18 +0300

    changed permision to docker socker on EC2 instance

--- terraform/entry-script.sh
+++ terraform/entry-script.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 # Install Docker
-sudo yum update -y
-sudo amazon-linux-extras install docker
-sudo groupadd docker
-sudo usermod -aG docker $USER
-sudo chmod 666 /var/run/docker.sock
-sudo docker permission for linux ec2
+sudo yum update -y && sudo yum install -y docker
 sudo systemctl start docker
+sudo usermod -aG docker ec2-user
+sudo chmod 666 /var/run/docker.sock
 
-# Install docker-compose
-sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+# install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
