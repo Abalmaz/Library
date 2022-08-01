f13c3ccb6b24ee0baf069d294fb9b9a3206b20c8
Yana Abalmaz <yana.abalmaz@gmail.com>
2022-08-01 19:45:57 +0300

    Added clear images functionality

--- script.groovy
+++ script.groovy
@@ -1,26 +1,12 @@
-def clearImage(){
-    script.sh "docker stop $(docker ps -q) || docker rm $(docker ps -a -q) || docker rmi $(docker images -q | tail -n +2)"
-}
-
-def dockerLogin(){
+def buildImage(){
+    echo "building..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')])
     {
-            sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh "docker stop $(docker ps -q) || docker rm $(docker ps -a -q) || docker rmi $(docker images -q | tail -n +2)"
+        echo "Build the Docker image..."
+        sh "docker build -t $IMAGE_NAME ."
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh "docker push $IMAGE_NAME"
     }
-}
-
-def dockerBuild(String imageName){
-    script.sh "docker build -t $imageName ."
-}
-
-def dockerPush(String imageName){
-    script.sh "docker push $imageName"
-}
-
-def buildImage(){
-//     clearImage()
-    dockerLogin()
-    dockerBuild(IMAGE_NAME)
-    dockerPush(IMAGE_NAME)
 }
 return this
