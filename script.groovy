3dc00bf85b763bc2c5facc6b4ce4cedf3c6b02ff
Yana Abalmaz <yana.abalmaz@gmail.com>
2022-08-01 19:47:16 +0300

    Added clear images functionality

--- script.groovy
+++ script.groovy
@@ -2,7 +2,7 @@ def buildImage(){
     echo "building..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')])
     {
-        sh "docker stop $(docker ps -q) || docker rm $(docker ps -a -q) || docker rmi $(docker images -q | tail -n +2)"
+//         sh "docker stop $(docker ps -q) || docker rm $(docker ps -a -q) || docker rmi $(docker images -q | tail -n +2)"
         echo "Build the Docker image..."
         sh "docker build -t $IMAGE_NAME ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
