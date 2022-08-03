54616e424c6613b4b5edb14cc80e6e420787cd47
Yana Abalmaz <yana.abalmaz@gmail.com>
2022-08-03 16:14:09 +0300

    temp

--- script.groovy
+++ script.groovy
@@ -15,7 +15,7 @@ def pushImage(){
 }
 
 def runTest(){
-    sh "docker run postgres:9.6 -e  POSTGRES_HOST_AUTH_METHOD=trust --rm"
+    sh "docker run postgres:9.6 -e POSTGRES_HOST_AUTH_METHOD=trust --rm"
     sh "docker run $IMAGE_NAME"
     sh "docker exec -it $IMAGE_NAME ./manage.py jenkins"
 }
